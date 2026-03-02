6888d5fce4e9bd11e52408850adb8a1f18f8987e
ngoclong-2004 <ngoclong9194@gmail.com>
2026-03-02 21:55:46 +0700

    bai day 7

--- index.js
+++ index.js
@@ -1,13 +1,36 @@
-const friends =[ "khang" , "an" , "thiện" ]
-friends.push("bảo")
-console.log(friends)
-console.log(friends.length)
-console.log(friends[0])
-friends.unshift("toàn")
-console.log(friends)
-friends.splice(4 , 1)
-console.log(friends)
-friends.splice(1 ,1 ,"thịnh")
-console.log(friends)
-for(let i = 0 ;i<friends.length; i++)
-    console.log(friends[i])
+//const friends =[ "khang" , "an" , "thiện" ]
+//friends.push("bảo")
+//console.log(friends)
+//console.log(friends.length)
+//console.log(friends[0])
+//friends.unshift("toàn")
+//console.log(friends)
+//friends.pop()
+//console.log(friends)
+//friends.splice(1 ,1 ,"thịnh")
+//console.log(friends)
+//for(let i = 0 ;i<friends.length; i++)
+ //   console.log(friends[i])
+
+
+
+
+
+
+
+
+function countValidCases(memoryUsages, limit) {
+    let count = 0;
+    for (let i = 0; i < memoryUsages.length; i++) {
+        if (memoryUsages[i] <= limit) {
+            count++;
+        }
+    }
+    return count;
+}
+const memoryUsages = [32, 45, 28, 50, 64, 20];
+const limit = 40;
+console.log(countValidCases(memoryUsages, limit));
+
+
+
